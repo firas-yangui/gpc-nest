7e6d7fc4893f4dabd2138edec0a8637cb98bdd1e
X182096 <anouer.hammami-ext@socgen.com>
2020-01-09 12:54:01 +0100

    feat(user api): add thirdparties relations to user entity

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
   ) {}
 
   async getUserByEmail(email: string): Promise<User> {
-    const user = await this.userRepository.findOne({ email });
+    const user = await this.userRepository.findOne({ where: { email: email }, relations: ['thirdParty', 'maxReadThirdParty', 'maxEditThirdParty'] });
     if (!user) {
       throw new NotFoundException(`User with email ${email} not found`);
     }
