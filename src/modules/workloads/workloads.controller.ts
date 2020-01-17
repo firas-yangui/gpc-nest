6d3edefb165706fccba587c24a5d745a147b4d6f
X182096 <anouer.hammami-ext@socgen.com>
2020-01-17 15:00:03 +0100

    feat(total amounts in periods): update route

--- src/modules/workloads/workloads.controller.ts
+++ src/modules/workloads/workloads.controller.ts
@@ -1,19 +1,17 @@
 import { Controller, Get, Param, ParseIntPipe, Req } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiImplicitHeader } from '@nestjs/swagger';
 import { Request } from 'express';
 import { WorkloadsService } from './workloads.service';
-import { PeriodType, TotalAmountForThirdpartyRootData } from './../interfaces/common-interfaces';
+import { PeriodType } from './../interfaces/common-interfaces';
 @Controller('workloads')
 export class WorkloadsController {
   constructor(private readonly workloadsService: WorkloadsService) {}
 
-  @Get('/total-amount/:periodType/:thirdpartyRootId')
-  async getUserByEmail(
+  @Get('/total-amount/:thirdpartyRootId')
+  async getTotalByPeriodTypesAndBusinessPlan(
     @Req() request: Request,
     @Param('periodType') periodType: PeriodType,
     @Param('thirdpartyRootId', new ParseIntPipe()) thirdpartyRootId: number,
-  ): Promise<TotalAmountForThirdpartyRootData> {
-    const user = await this.workloadsService.getTotalAmountForThirdpartyRoot(request, periodType, thirdpartyRootId);
-    return user;
+  ) {
+    return await this.workloadsService.getTotalByPeriodTypesAndBusinessPlan(request, thirdpartyRootId);
   }
 }
