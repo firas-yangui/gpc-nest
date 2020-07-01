c04160db7607365d7a4efae4210311118f49638d
Anouer HAMMAMI <anouer.hammami-ext@socgen.com>
2020-07-01 15:35:35 +0200

    update rmq connection

--- src/config/rabbitmq-config.service.ts
+++ src/config/rabbitmq-config.service.ts
@@ -8,7 +8,7 @@ export class ConfigService implements RabbitMQOptionsFactory {
   port = process.env.RMQ_PORT || 5672;
   user = process.env.RMQ_USER || 'guest';
   password = process.env.RMQ_PASSWORD || 'guest';
-  vhost = process.env.RMQ_VHOST || '/';
+  vhost = process.env.RMQ_VHOST || '';
 
   createRabbitMQConnectOptions(): RabbitMQConnectOptions | string {
     return `${this.protocol}://${this.user}:${this.password}@${this.host}:${this.port}/${this.vhost}`;
