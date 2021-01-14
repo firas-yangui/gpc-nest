c93a7b39824966a3c19711cede63edae2046f45e
Anouer Hammami <anouer.hammami-ext@socgen.com>
2021-01-14 13:11:06 +0100

    fix(pyramid):updare mapping datalake gpc orgas

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,6 +10,12 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   schema: process.env.DATABASE_SCHEMA || process.env.TYPEORM_DATABASE_SCHEMA || 'public',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: false,
+  migrationsTableName: 'gpc_migrations',
+  migrations: ['migrations/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'migrations/*{.ts,.js}',
+  },
+
 };
 
 export default typeOrmConfig;
