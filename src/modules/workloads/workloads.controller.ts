d9d0d1e4f94b0df3d5ce434b00e507786935aa50
Mehdi <mehdi.fracso@socgen.com>
2021-03-02 17:06:00 +0100

    refactor: added filters entities to query

--- src/modules/workloads/workloads.controller.ts
+++ src/modules/workloads/workloads.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Get, HttpException, HttpStatus, Logger, Param, Post } from '@nestjs/common';
 import { WorkloadsService } from './workloads.service';
-import { PeriodType } from './../interfaces/common-interfaces';
 import { ApiUseTags } from '@nestjs/swagger';
 import { Workload } from './workload.entity';
 
@@ -10,25 +8,28 @@ import { Workload } from './workload.entity';
 export class WorkloadsController {
   constructor(private readonly workloadsService: WorkloadsService) {}
 
-  @Get('/total-amount/:thirdpartyRootId')
-  async getTotalByPeriodTypesAndBusinessPlan(
-    @Req() request: Request,
-    @Param('periodType') periodType: PeriodType,
-    @Param('thirdpartyRootId', new ParseIntPipe()) thirdpartyRootId: number,
-  ) {
-    return await this.workloadsService.getTotalByPeriodTypesAndBusinessPlan(request, thirdpartyRootId);
+  @Get()
+  findAll(): Promise<Workload[]> {
+    return this.workloadsService.find({});
+  }
+
+  @Post('/total-amount')
+  async getTotalByPeriodTypesAndBusinessPlan(@Body() body: { [key: string]: number }) {
+    try {
+      const { thirdpartyRootId } = body;
+      if (!thirdpartyRootId) throw new HttpException('Please set the thirdpartyrootId value', HttpStatus.BAD_REQUEST);
+
+      return await this.workloadsService.getTotalByPeriodTypesAndBusinessPlan(body, thirdpartyRootId);
+    } catch (error) {
+      Logger.error(error);
+
+      return [];
+    }
   }
 
   @Get(':id')
-  findById(@Param('id') id: number): Promise<Workload> {
+  async findById(@Param('id') id: number): Promise<Workload> {
     return this.workloadsService.findOne({ id }).catch(err => {
-      throw new HttpException(err, HttpStatus.BAD_REQUEST);
-    });
-  }
-
-  @Get(':id/getPartners')
-  getSubServices(@Param('id') id: number): Promise<SubService[]> {
-    return this.workloadsService.getWorkloads({ id }).catch(err => {
       throw new HttpException(err, HttpStatus.BAD_REQUEST);
     });
   }
