60a0aa84d73cf2a977a58012c04718f8fae4bb2c
hubert <hubert.burot-de-carcouet@socgen.com>
2021-03-23 17:58:45 +0100

    feat: homeMessage post

--- src/modules/homeMessage/homeMessage.controller.ts
+++ src/modules/homeMessage/homeMessage.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Header, Logger, Param } from '@nestjs/common';
-import { ApiResponse, ApiImplicitParam } from '@nestjs/swagger';
+import { Controller, Post, Get, Header, Logger, Param, HttpCode, Body } from '@nestjs/common';
+import { ApiResponse, ApiImplicitParam, ApiImplicitBody, ApiOperation, ApiForbiddenResponse } from '@nestjs/swagger';
 
 import { ErrorModel } from '../exceptions-handler/error-model';
 import { HomeMessage } from './homeMessage.entity';
+import { HomeMessageDto } from './homeMessage.dto';
 import { HomeMessageService } from './homeMessage.service';
 
 @Controller('homeMessage')
@@ -33,8 +34,37 @@ export class HomeMessageController {
     type: ErrorModel,
     isArray: false,
   })
-  async getLatest(@Param('perimeterId') perimeterId): Promise<HomeMessage[]> {
+  async getLatest(@Param('perimeterId') perimeterId): Promise<HomeMessage> {
     Logger.log('Get homepage message', 'HomeMessageController');
     return await this.homeMessageService.getLatest(perimeterId);
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  @Header('Content-Type', 'application/json')
+  @Header('Accept-Charset', 'utf-8')
+  // @SgConnectScopes('api.soapoc.write')
+  @ApiImplicitBody({ name: 'message', description: 'Admin message', type: 'string' })
+  @ApiImplicitBody({ name: 'perimeterId', description: 'User perimeter ID', type: 'number' })
+  @ApiOperation({
+    description: 'Create Home Message',
+    title: 'Create',
+    operationId: 'POST /homeMessage',
+  })
+  @HttpCode(201)
+  @ApiResponse({
+    status: 201,
+    description: 'Create Home Message',
+    type: HomeMessage,
+    isArray: false,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request', type: ErrorModel, isArray: false })
+  @ApiResponse({ status: 401, description: 'Unauthorized', type: ErrorModel, isArray: false })
+  @ApiResponse({ status: 500, description: 'Internal server error', type: ErrorModel, isArray: false })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  async create(@Body() homeMessageDto: HomeMessageDto): Promise<HomeMessage> {
+    Logger.log('Create HomeMessage', 'HomeMessageController');
+    const newMessage = await this.homeMessageService.create(homeMessageDto);
+    return newMessage;
   }
 }
--- src/modules/homeMessage/homeMessage.service.ts
+++ src/modules/homeMessage/homeMessage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, NotFoundException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { HomeMessage } from './homeMessage.entity';
 import { HomeMessageRepository } from './homeMessage.repository';
+import { HomeMessageDto } from './homeMessage.dto';
+import { HomeMessage } from './homeMessage.entity';
 
 @Injectable()
 export class HomeMessageService {
   constructor(@InjectRepository(HomeMessageRepository) private readonly homeMessageRepository: HomeMessageRepository) {}
 
-  public async getLatest(perimeter: number): Promise<HomeMessage[]> {
+  async getLatest(perimeter: number): Promise<HomeMessage> {
     try {
       const homeMessage = await this.homeMessageRepository.find({
         where: {
@@ -25,9 +26,15 @@ export class HomeMessageService {
       if (!homeMessage) {
         throw new NotFoundException(`Home message not found`);
       }
-      return homeMessage;
+      return homeMessage[0];
     } catch (error) {
       Logger.log(error);
     }
+  }
+
+  async create(homeMessageDto: HomeMessageDto): Promise<HomeMessage> {
+    const data = { message: homeMessageDto.message, gpcappsettings: { id: homeMessageDto.perimeterId } };
+    const newMessage = await this.homeMessageRepository.save(data);
+    return newMessage;
   }
 }
