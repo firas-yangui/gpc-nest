ecbd2f7550c08be087820aa6f47d96f7c2bce491
Mehdi <mehdi.fracso@socgen.com>
2021-04-28 16:51:29 +0200

    feat: added period fetch route

--- src/modules/periods/periods.service.ts
+++ src/modules/periods/periods.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, NotFoundException, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PeriodRepository } from './period.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PeriodType } from './../interfaces/common-interfaces';
 import { Period } from './period.entity';
 import { createQueryBuilder, Like } from 'typeorm';
 import * as moment from 'moment';
-import { Object } from 'lodash';
 
 interface IfindOneInAppSettings {
   type: string;
@@ -24,10 +23,6 @@ export class PeriodsService {
     return await this.periodRepository.findAndCount();
   }
 
-  async find(options: object): Promise<Period[]> {
-    return await this.periodRepository.find(options);
-  }
-
   async findOne(options: object): Promise<Period> {
     return await this.periodRepository.findOne(options);
   }
@@ -40,6 +35,22 @@ export class PeriodsService {
       .andWhere('p.year = :year', { year: options.year })
       .andWhere('p.month = :month', { month: options.month })
       .getOne();
+  }
+
+  async find(appSettings: number, options: any): Promise<any> {
+    try {
+      return await createQueryBuilder()
+        .from(Period, 'period')
+        .select('period.*')
+        .addSelect('pas.iscampaignperiod', 'iscampaignperiod')
+        .leftJoin('period.periodappsettings', 'pas')
+        .leftJoin('pas.gpcappsettings', 'gas')
+        .where('gas.id = :gpcappsettingsid', { gpcappsettingsid: appSettings })
+        .getRawMany();
+    } catch (error) {
+      Logger.error(error, 'PeriodsService');
+      return [];
+    }
   }
 
   async getPeriodsByType(type: PeriodType): Promise<Period[]> {
