ab09f2b5cf7af8cfcd9d1056e46be6676606b27e
shuwen <nishuwen@MacBook-Pro-de-nishuwen.local>
2021-07-08 11:32:13 +0200

    generic service workloads : work in progress

---
+++ src/modules/workloads/interface/synthesis-filter.interface.ts
@@ -0,0 +1,10 @@
+export interface SynthesisFilter {
+  code: string;
+  description: string;
+  domaine: string;
+  thirdparties: number[];
+  portfolios: number[];
+  plans: number[];
+  subnatures: number[];
+  partners: number[];
+}
---
+++ src/modules/workloads/interface/workload-tree-data-item.interface.ts
@@ -0,0 +1,9 @@
+export interface WorkloadTreeDataIItem {
+  plan: string;
+  ssName: string;
+  ssCode: string;
+  snName: string;
+  svrName: string;
+  wCode: string;
+  wStatus: string;
+}
