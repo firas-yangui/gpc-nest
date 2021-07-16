a05f50af3220d6f3d48ee2afbf58225f86c54ad2
rodolphe <rodolphe.poon-ext@socgen.com>
2021-07-16 15:56:42 +0200

    fix(cron): remove local import log

--- src/modules/tasks/tasks-manager.service.ts
+++ src/modules/tasks/tasks-manager.service.ts
@@ -165,7 +165,6 @@ export class TasksService {
    */
   @Cron(CronExpression.EVERY_10_SECONDS)
   async importFromLocal() {
-    this.logger.log(`Start Import from Local`);
     if (includes(secureEnvs, process.env.NODE_ENV)) return false;
     const inProgressImports = await this.rawAmountsService.findAll();
     if (inProgressImports.length) return false;
@@ -184,6 +183,5 @@ export class TasksService {
       const lines = readFileSync(path.join(receptiondir, file.name));
       await this.parseFile(lines, file.name);
     });
-    this.logger.log(`end Import`);
   }
 }
